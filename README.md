189d3ca05af80da70eb98148cb9e262bd2b80ee5
Emulie Chhor <emulie.chhor@hotmail.com>
2020-12-03 10:43:44 -0500

    added readme and timetracker file

---
+++ README.md
@@ -0,0 +1,25 @@
+# Schedule-Automation
+
+## Motivation
+
+I want to automate my planning system using python to produce the excel sheet
+I need.
+
+## Spreadsheet to Implement
+
+- [ ] Time Tracker: script that create a new file each month
+- [ ] Schedule: given the the lecture and project list, produce a spreadsheet
+	  that indicates the course material that needs to be done for a given
+	  week.
+- [ ] Weekly-Daily Plan: [to do]
+
+## Modules
+
+* openpyxl: excel module
+* streamlit: view weekly-monthly stats
+* 
+
+## Ressources
+
+- [ ] How to Automate tasks on windows: https://www.techradar.com/news/software/applications/how-to-automate-tasks-in-windows-1107254
+- [ ] openpyxl documentation: https://openpyxl.readthedocs.io/en/stable/tutorial.html
---
+++ timetracker.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+"""
+    0. Create the document
+        a. Generate its name generation: Time Tracker - [Month Year]
+        b. Generate Sheet for all weeks in the month
+        c. Automate the file creation with windows tasks automation
+
+    1. Create a legend 
+        a. Properties
+            - name (string): name of the tag
+            - color (string): color code for the tag
+            - id (string): ??? an id if we can't recognize the tag by its color
+            - description (string): description of the tag
+    2. Generate the template with date and time
+    3. Create weekly stats with streamlit
+
+"""
+import openpyxl
+import os
+
+
+from openpyxl import Workbook
+
+from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.styles import colors
+from openpyxl.cell import Cell
+
+
+
+legend = {'Active Recall': '#ff0000', 
+          'TPs and Exercices': '#ff0000',
+          #  'Slides':'#151',
+          #  'Lecture Review':'#134',
+          #  'Unanswered Questions':'#346',
+          #  'Coding':'#236',
+          #  'Sleep':'#345',
+          #  'Management': '#456',
+          #  'Wasted Time': '#ff0000',
+          #  'Chores + Toilettes': '#876543',
+          #  'Eating': '#324',
+          'Training':'#0999ff'}
+
+days_of_week = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi',
+        'Dimanche']
+
+row_title = 4
+
+# path where the file will be saved
+path = "C:/Users/emuli/OneDrive - Universite de Montreal/Bac-Maths-Info/Organization Documents/Time Tracker"
+
+
+def main():
+    wb = Workbook() # create workbook
+    ws = wb.active
+
+    # generate week tab
+    for i in range(4):
+        ws = wb.create_sheet(f"Week {i+1}")
+    
+        # Add legend to tab
+        ws['K4'] = "Légende"
+        for i, tag in enumerate(legend):
+            #  line = 6+i #6 is the cell where the legend begins
+            ws[f'K{6+i}'] = f"{tag}"  
+            #  background_color = openpyxl.styles.colors.Color(rgb = '00FF000')
+            #  print(legend[tag][1:])
+            background_color = openpyxl.styles.colors.Color(rgb = legend[tag][1:])
+            fill_color = openpyxl.styles.fills.PatternFill(patternType='solid',\
+                    fgColor=background_color)
+            ws[f'K{6+i}'].fill = fill_color
+
+        # Add days of week
+        for i, day in enumerate(days_of_week):
+            _ = ws.cell(column = i + 2, row = row_title, value = day)
+
+        
+        # Add timestamp
+        
+    # rename workbook - Month Year
+
+    # save file
+    wb.save(os.path.join(path, 'Time Tracker - [Month Year].xlsx'))
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
