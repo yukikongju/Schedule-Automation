c4a3ac787fbbef34821c15799fe5efa7d76ed413
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-04 04:25:27 -0500

    renamed Spreadsheet to spreadsheet

--- Course-Prep-Automation/Spreadsheet.py
+++ Course-Prep-Automation/Spreadsheet.py
@@ -3,7 +3,8 @@
 from openpyxl import Workbook
 
 class Spreadsheet:
-    def __init__(self, session_name, material_path, courses_lectures_list, courses_names):
+    def __init__(self, session_name, material_path, courses_lectures_list,
+            courses_names):
         self.session_name = session_name               # name of the session
         self.material_path = material_path     # dir with all classes for the session
         self.courses_lectures_list = courses_lectures_list
