d1af1a059ecbf3be77576b0018c4b425cd0bc7a5
Emulie Chhor <emulie.chhor@hotmail.com>
2021-01-14 23:46:45 -0500

    fixed bug for course name

--- Course-Prep-Automation/manager.py
+++ Course-Prep-Automation/manager.py
@@ -10,7 +10,7 @@ class SessionManager:
         self.course_path = course_path
         self.courses = self.fetch_courses()
         print(self.courses)
-        #  self.generate_spreadsheets
+        self.generate_spreadsheets
 
     def fetch_courses(self):
         """ fetch all courses from .txt files in path 
@@ -21,24 +21,32 @@ class SessionManager:
             """ Return course name """
             return txt_file.replace(".txt", "")
 
-        def fetch_and_generate_course(txt_file):
+        def fetch_and_generate_course(self, txt_file):
             """ Return Course object """
             course_name = fetch_course_name(txt_file)
             lectures_list = []
             with open(txt_file) as f:
                 lectures_list = [line.strip() for line in f]
             f.close()
-            return Course(self.name, lectures_list)
+            return Course(course_name, lectures_list)
 
         courses = []
         os.chdir(self.course_path) # change directory
         for txt_file in glob.glob("*.txt"):
-            course = fetch_and_generate_course(txt_file)
+            course = fetch_and_generate_course(self, txt_file)
             courses.append(course)
         return courses
 
     def generate_spreadsheets(self):
+        # TODO: refractor spreadsheet to use manager instead → change spreadsheet constructor and refractor code
+        spreadsheets = []
         # generate_active_recall_spreadsheet
+        #  ar_spreadsheet = ActiveRecall(self.courses)
+        #  spreadsheets.append(ar_spreadsheet)
+
+        # generate schedule spreadsheet
+        #  schedule_spreadsheet = Schedule(self.courses)
+
         #  ar_spreadsheet = ActiveRecall(session_name, material_path,
         #          courses_lectures_list, courses_names)
 
